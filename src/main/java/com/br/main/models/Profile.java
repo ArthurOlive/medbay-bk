4c6d3b8081feb910e89dfbbcec32990a01fd2776
Arthur Oliveira <arthuroliveira909.av@gmail.com>
2022-03-28 20:57:45 -0300

    feat. Profile e migração

---
+++ src/main/java/com/br/main/models/Profile.java
@@ -0,0 +1,70 @@
+package com.br.main.models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Profile implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="profile_generator")
+	@SequenceGenerator(name="profile_generator", sequenceName="profile_seq", allocationSize=1)
+    private long id;
+    private String name;
+    private LocalDate birth;
+    private String document;
+    private GenderEnum gender;
+    @OneToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+    private Address address;
+    @OneToMany(mappedBy="profile", cascade=CascadeType.ALL, fetch=FetchType.LAZY )
+    private List<MetaData> medaData; 
+
+    public Address getAddress() {
+        return address;
+    }
+    public String getDocument() {
+        return document;
+    }
+    public LocalDate getBirth() {
+        return birth;
+    }
+    public GenderEnum getGender() {
+        return gender;
+    }
+    public long getId() {
+        return id;
+    }
+    public List<MetaData> getMedaData() {
+        return medaData;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public void setDocument(String document) {
+        this.document = document;
+    }
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+    public void setGender(GenderEnum gender) {
+        this.gender = gender;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setMedaData(List<MetaData> medaData) {
+        this.medaData = medaData;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/resources/db/migration/V2022032804__profile.sql
@@ -0,0 +1,21 @@
+DROP SEQUENCE IF EXISTS public.profile_seq;
+CREATE SEQUENCE public.profile_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+DROP TABLE IF EXISTS public.profile;
+CREATE TABLE public.profile (
+    id bigint primary key,
+    name varchar(255),
+    birth date,
+    document varchar(255) unique,
+    address_id bigint,
+    gender int,
+    CONSTRAINT fk_address_id FOREIGN KEY (address_id)
+        REFERENCES public.address (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION 
+);
