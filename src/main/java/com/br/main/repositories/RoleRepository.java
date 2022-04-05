0585452252f07fa24209911df3363dd0d9445626
Arthur Oliveira <arthuroliveira909.av@gmail.com>
2022-04-04 23:20:58 -0300

    feat. Role service e repository

---
+++ src/main/java/com/br/main/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.br.main.repositories;
+
+import com.br.main.models.Role;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+    
+    public Role findByName(String name);
+}
---
+++ src/main/java/com/br/main/services/RoleService.java
@@ -0,0 +1,30 @@
+package com.br.main.services;
+
+import com.br.main.controllers.dtos.RoleEnum;
+import com.br.main.models.Role;
+import com.br.main.repositories.RoleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+    
+    public Role findByRoleEnum(RoleEnum role) {
+        
+        if (role.equals(RoleEnum.MEDICO)) {
+            return roleRepository.findByName("MEDICO");
+        } 
+        if (role.equals(RoleEnum.ATENDENTE)) {
+            return roleRepository.findByName("ATENDENTE");
+        }
+        if (role.equals(RoleEnum.PACIENTE)) {
+            return roleRepository.findByName("PACIENTE");
+        }
+
+        return null;
+    }
+}
