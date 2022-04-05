5e7ca3997a83fe16860bca0daae2e814f6c864dd
Arthur Oliveira <arthuroliveira909.av@gmail.com>
2022-04-04 23:19:10 -0300

    feat. UserCustomDetails sobrepondo o UserDetails padrão do spring

---
+++ src/main/java/com/br/main/config/UserCustomDetails.java
@@ -0,0 +1,74 @@
+package com.br.main.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.br.main.models.UserSystem;
+
+public class UserCustomDetails implements UserDetails {
+
+    private UserSystem user;
+    private String username;
+    private String password;
+    private String role;
+
+    public UserCustomDetails(UserSystem user) {
+
+        this.user = user;
+        this.username = user.getAuth().getUsername();
+        this.password = user.getAuth().getPassword();
+        this.role = user.getRole().getName();
+
+    }
+
+    public UserSystem getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        if (this.role == null) return null;
+
+        Collection<GrantedAuthority> auths = new ArrayList<>();
+
+        auths.add(new SimpleGrantedAuthority("ROLE_" + role));
+        
+        return auths;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
