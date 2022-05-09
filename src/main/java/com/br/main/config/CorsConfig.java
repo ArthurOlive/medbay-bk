12a96aa99671f5bc0973dd4ab81e674607317841
Dylan <dylan.oli@hotmail.com>
2022-05-09 18:04:50 -0300

    make it better

--- src/main/java/com/br/main/config/CorsConfig.java
+++ src/main/java/com/br/main/config/CorsConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
-    } 
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://medbay-web-client.pages.dev")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 
 }
