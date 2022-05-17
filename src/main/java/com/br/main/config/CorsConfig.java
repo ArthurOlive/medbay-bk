aff3300a40bb590cce72b4e400e9c34d5ac68667
Dylan <dylan.oli@hotmail.com>
2022-05-16 23:12:19 -0300

    integração medicos

--- src/main/java/com/br/main/config/CorsConfig.java
+++ src/main/java/com/br/main/config/CorsConfig.java
@@ -10,8 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://medbay-web-client.pages.dev")
+                .allowedOrigins("http://localhost:3000", "https://medbay-web-client.pages.dev")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 
